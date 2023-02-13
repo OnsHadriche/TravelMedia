def4c9ed3b2f0b3da149f67152b4e9768cf9810d
Ons Hadriche <ounshadriche@gmail.com>
2023-02-13 19:32:28 +0100

    Set up App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import UpdateEvent from "./pages/UpdateEvent";
+import UpdateHotel from "./pages/UpdateHotel";
+import UpdatePack from "./pages/UpdatePack";
 import CreatePageAgency from "./pages/CreatePageAgency";
 import DetailsHotel from "./pages/DetailsHotel";
 import DetailsPage from "./pages/DetailsPage";
-// import DetailsHotel from "./pagenpm s/DetailsHotel";
+
 import Home from "./pages/Home";
 import Hotels from "./pages/Hotels";
 import Profile from "./pages/Profile";
@@ -39,6 +42,9 @@ function App() {
           <Route path="/profile" exact element={<Profile/>} />
           <Route path="/create-page" exact element={<CreatePageAgency/>} />
           <Route path="/page/:id" exact element={<DetailsPage/>} />
+          <Route path = "/update-hotel/:id" exact element = {<UpdateHotel/>}/>
+          <Route path = "/update-pack/:id" exact element = {<UpdatePack/>}/>
+          <Route path = "/update-event/:id" exact element = {<UpdateEvent/>}/>
           {/* <CustomRoute path="/" exact component={<Home />} />
           <CustomRoute path="/hotels" exact component={<Hotels />} />
           <PublicRoute path="/register" exact component={<Register />} />
