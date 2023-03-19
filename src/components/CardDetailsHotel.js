355705375d5efc2e8fc6cae4cede5a3cb70218a5
Ons Hadriche <ounshadriche@gmail.com>
2023-03-20 00:13:14 +0100

    Create cardDetails

--- src/components/CardDetailsHotel.js
+++ src/components/CardDetailsHotel.js
@@ -1,21 +1,12 @@
-import React, { useState } from "react";
-import StarIcon from "@mui/icons-material/Star";
-import { Rating } from "@mui/material";
+import React from "react";
 import { deepOrange } from "@mui/material/colors";
 import Avatar from "@mui/material/Avatar";
-import { MdPool } from "react-icons/md";
-import { AiOutlineWifi, AiFillSafetyCertificate } from "react-icons/ai";
-import { FaCocktail, FaUmbrellaBeach } from "react-icons/fa";
-import { GrRestaurant } from "react-icons/gr";
-import { BsCurrencyExchange } from "react-icons/bs";
-import AcUnitIcon from "@mui/icons-material/AcUnit";
-import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
-import DateRangePicker from "./checkDate";
-import { useDispatch, useSelector } from "react-redux";
-import { addRatingToHotel } from "../redux/actions/hotelActions";
+
+import { useDispatch } from "react-redux";
+
 import { useParams } from "react-router-dom";
 // import DateRate from "./dateRate";
+import "../styles/cardHotel.css"
 
 
 
@@ -24,114 +15,39 @@ import { useParams } from "react-router-dom";
 function CardDetailsHotel({hotelSelected}) {
     const dispatch = useDispatch();
     const { id } = useParams();
-    const { info } = useSelector((state) => state.user);
+   
   
     console.log("=====================================");
-    const [value, setValue] = useState(hotelSelected?.userReview?.note);
-    console.log(value);
-  
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-  
-      dispatch(addRatingToHotel(id, { note: value, user: info._id, hotel: id }));
-    };
-  
+    console.log();
+
     return (
       <>
-        <div className="d-flex align-items-center">
-          <h1>{hotelSelected?.hotel?.title} </h1>
+        <div className="d-flex align-items-center row hotelDetailsPrice">
+     
+          <h3 className="col-8">{hotelSelected?.title} </h3>
         
           {/* <form onSubmit={handleSubmit}> */}
-          <form className="row">
-            <Rating
-              name="simple-controlled"
-              value={value}
-              precision={1}
-              onChange={(newValue) => {
-                setValue(newValue);
-              }}
-              emptyIcon={
-                <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
-              }
-              className="ms-1 col"
-            />
-            <button type="submit" className="btn btn-primary col" onClick={handleSubmit}>
-              Submit
-            </button>
-          </form>
+          <button type="submit" className=" col-4">
+              Reserve Now 
+          </button>
+     
         </div>
-  
-        <div className="d-flex mt-3 align-items-start">
+         <div className="d-flex mt-3 align-items-start">
           <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
           <h6 className="m-2">Hosted By Twinci travel agency </h6>
         </div>
+        <div>
+   
+        </div>
+  
+    
         <div className="mt-4">
           <p>
             {hotelSelected?.details}
           </p>
         </div>
-        <div>
-          <h6>Services & equipements</h6>
-          <div>
-            <Stack
-              direction="row"
-              spacing={1}
-              style={{ listStyleType: "none", flexWrap: "wrap" }}
-              className="row-cols-3 row-cols-sm-4 row-cols-md-5"
-            >
-              <Chip
-                label="Bar"
-                icon={<FaCocktail />}
-                variant="outlined"
-                className="col mb-1"
-              />
-              <Chip
-                label="Free Wifi"
-                icon={<AiOutlineWifi />}
-                variant="outlined"
-                className="col mb-1"
-              />
-              <Chip
-                label="Safe"
-                icon={<AiFillSafetyCertificate />}
-                variant="outlined"
-                className="col mb-1"
-              />
-              <Chip
-                label="Air conditioner"
-                icon={<AcUnitIcon />}
-                variant="outlined"
-                className="col mb-1"
-              />
-              <Chip
-                label="Swimming pool"
-                icon={<MdPool />}
-                variant="outlined"
-                className="col mb-1"
-              />
-              <Chip
-                label="Beach"
-                icon={<FaUmbrellaBeach />}
-                variant="outlined"
-                className="col mb-1"
-              />
-              <Chip
-                label="Restaurant"
-                icon={<GrRestaurant />}
-                variant="outlined"
-                className="col mb-1"
-              />
-              <Chip
-                label="Exchange"
-                icon={<BsCurrencyExchange />}
-                variant="outlined"
-                className="col mb-1"
-              />
-            </Stack>
-          </div>
-        </div>
         
+      
       </>
     );
   }
